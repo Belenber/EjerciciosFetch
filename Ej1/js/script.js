cc29cb896fddc2157c962f61aca7dd0f9a7d4403
franberenguer <106634071+Belenber@users.noreply.github.com>
2022-08-18 20:35:00 -0300

    hola

--- Ej1/js/script.js
+++ Ej1/js/script.js
@@ -19,3 +19,16 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL)
+ .then (function (response){
+  return response.json();
+})
+.then(function(data){
+
+
+  showData (data.students);
+
+ 
+});
+
